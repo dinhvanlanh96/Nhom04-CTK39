e09b49ddab63180195a62963d6611a39f45da2bd
dinhvanlanh96 <dinhvanlanh1996@gmail.com>
2019-03-18 11:06:26 +0700

    Văn Lành
    Thiết Lập Project

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './Components/App/App.jsx';
+import * as serviceWorker from './Service/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
