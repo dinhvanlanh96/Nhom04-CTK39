68d736b26c91b4ed00aa94d475e8253c8c0e6e3b
dinhvanlanh96 <dinhvanlanh1996@gmail.com>
2019-03-18 11:44:35 +0700

    Văn Lành
    Thiết Lập Giao Diện

--- Server/routes/web.php
+++ Server/routes/web.php
@@ -28,45 +28,6 @@ Route::group(['prefix' => 'account'],function (){
 Route::group(['prefix' => 'Dashboards'],function (){
      Route::get('/Dashboards','DashboardsController@index'); // Khu Trọ
 });
-//
-Route::group(['prefix' => 'Buildings'],function (){
-     Route::get('/Buildings','BuildingsController@index'); // Khu Trọ
-});
-//
-Route::group(['prefix' => 'Rooms'],function (){
-     Route::get('/Rooms','RoomsController@index'); // Phòng Trọ
-});
-//
-Route::group(['prefix' => 'Services'],function (){
-     Route::get('/Services','ServicesController@index'); //  Dịch Vụ
-});//
-Route::group(['prefix' => 'Devices'],function (){
-     Route::get('/Devices','DevicesController@index'); // Thiết Bị
-});//
 Route::group(['prefix' => 'Profiles'],function (){
      Route::get('/Profiles','ProfileController@index'); // Thông Tin Cá NHân
-});//
-Route::group(['prefix' => 'Units'],function (){
-     Route::get('/Units','UnitsController@index'); //
-});//
-Route::group(['prefix' => 'Renters'],function (){
-     Route::get('/Renters','RentersController@index'); //
-});//
-Route::group(['prefix' => 'Renter_histories'],function (){
-     Route::get('/Renter_histories','Renter_historiesController@index'); //
-});//
-Route::group(['prefix' => 'Responses'],function (){
-     Route::get('/Responses','ResponsesController@index'); //
-});//
-Route::group(['prefix' => 'Login'],function (){
-     Route::get('/Login','LoginController@index'); // Login
-});//
-Route::group(['prefix' => 'Deposits'],function (){
-     Route::get('/Deposits','DepositsController@index'); // Đặt Cọc Phòng
-});//
-Route::group(['prefix' => 'Leases'],function (){
-     Route::get('/Leases','LeasesController@index'); //
-});//
-Route::group(['prefix' => 'Formulas'],function (){
-     Route::get('/Formulas','LeasesController@index'); //
 });//
