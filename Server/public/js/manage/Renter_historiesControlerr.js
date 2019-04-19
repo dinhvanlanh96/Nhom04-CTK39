740909215363b4ba2c52e7a6ddc8ba52b7bf9b7c
dinhvanlanh96 <dinhvanlanh1996@gmail.com>
2019-04-19 08:49:48 +0700

    Văn Lành
    Delete

--- Server/public/js/manage/Renter_historiesControlerr.js
+++
@@ -1,5 +0,0 @@
-function Renter_historiesController (){
-      this.Renter_historiesMain=function(){
-          $('#Renter_histories-table').DataTable();
-    }
-}
--- Server/public/js/manage/ServicesController.js
+++
@@ -1,5 +0,0 @@
-function ServicesController (){
-      this.ServicesMain=function(){
-          $('#Services-table').DataTable();
-    }
-}
